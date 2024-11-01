fda64f03596e83b67508aee6f89f6f9add454ed3
wiederkf <wiederkf@biol.ethz.ch>
2024-11-01 17:29:00 +0100

    Add script for suppl figure 4d

---
+++ scripts/suppl-figure-4-d-organic-matter-growth.R
@@ -0,0 +1,29 @@
+# This script plots the growth curves for Sulfidibacter corallicola grown with coral-derived dissolved organic matter
+
+# Set directory
+setwd("C:/Users/wiederkf/Work Folders/Documents/Git_Projects/Reef_Microbiomics_DB/code")
+
+# Load packages
+require(data.table)
+require(tidyverse)
+require(ggforce)
+
+# Load data
+metadat<-fread("organic-matter-growth_metadata.csv",header=TRUE,data.table=FALSE) %>%
+  filter(!timepoint=="t8") %>% #abort rate high
+  filter(!organic_matter=="DOM") #not used in paper (coral-exuded dissolved organic matter)
+flowdat<-fread("organic-matter-growth_cell-counts.csv",header=TRUE,data.table=FALSE) %>%
+  filter(id %in% metadat$id)
+  
+# Append metadata to flow cytometry data
+dat<-flowdat %>%
+  left_join(metadat,by="id")
+
+# Plot growth curves
+ggplot(data=dat %>% filter(bacteria=="bac"),aes(x=hours,y=counts)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~organic_matter) +
+  scale_y_log10() +
+  theme_bw()
+ggsave(filename="organic-matter-growth_growth-curves.png",width=12,height=6,dpi=500)
